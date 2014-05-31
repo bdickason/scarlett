7fcc992cff21856f9b5cf8c0b655722e9b27034f
bdickason <dickason@gmail.com>
2014-05-31 19:41:24 -0400

    Can grab oauth2 access token

--- server/cfg/config.js
+++ server/cfg/config.js
@@ -4,9 +4,14 @@ exports.NODE_ENV = process.env.NODE_ENV || 'development';
 exports.HOSTNAME = process.env.HOSTNAME || 'localhost';
 exports.PORT = process.env.PORT || 3000;
 
+// Temporary - username/pass
 exports.MAIL_USERNAME = process.env.MAIL_USERNAME || '';
 exports.MAIL_PASSWORD = process.env.MAIL_PASSWORD || '';
 
-// Server - gmail
+// Gmail - Server
 exports.MAIL_HOST = process.env.MAIL_HOST || 'imap.gmail.com';
 exports.MAIL_PORT = process.env.MAIL_PORT || 993;
+
+// Gmail - OAuth2
+exports.CLIENT_ID = process.env.CLIENT_ID || '';
+exports.CLIENT_SECRET = process.env.CLIENT_SECRET || '';
--- server/index.js
+++ server/index.js
@@ -7,6 +7,9 @@ var bodyParser = require('body-parser');
 var favicon = require('serve-favicon');
 
 var MailListener = require('mail-listener2');
+var xoauth2 = require('xoauth2');
+var OAuth2 = require('oauth').OAuth2;
+
 
 module.exports.startServer = function() {
   app = express();
@@ -17,44 +20,111 @@ module.exports.startServer = function() {
 
 
   /* Basic funk-tionality */
-  var mailListener = new MailListener({
-    username: cfg.MAIL_USERNAME,
-    password: cfg.MAIL_PASSWORD,
-    host: cfg.MAIL_HOST,
-    port: cfg.MAIL_PORT,
-    markSeen: false,            // Do not mark e-mail as read (yet!)
-    fetchUnreadOnStart: false,  // Do not grab all e-mail at once, only new stuff.
-    attachments: false          // Do not grab attachments (yet!)
-  });
 
-  console.log(mailListener);
-  mailListener.start();
-
-  mailListener.on('server:connected', function() {
-    console.log('imap Connected');
-  });
-
-  mailListener.on("server:disconnected", function(){
-    console.log("imap Disconnected");
-  });
-
-  mailListener.on("error", function(err){
-    console.log(err);
-  });
-
-  mailListener.on("mail", function(mail, seqno, attributes){
-    // do something with mail object including attachments
-    console.log("emailParsed", mail);
-    // mail processing code goes here
-  });
-
-  mailListener.on("attachment", function(attachment){
-    console.log('wtf, an attachment?!');
-    console.log(attachment.path);
-  });
-
+ 
   /* API Routes */
-  app.get('*', function(req, res) {
-    res.send('success');
+  app.get('/', function(req, res) {
+
+    var authorizeConfig = {
+      response_type: 'code',
+      redirect_uri: 'http://localhost:3000/callback',
+      scope: 'https://mail.google.com/',
+      access_type: 'offline'
+    };
+
+    var server = 'https://accounts.google.com/o/';
+
+    var oauth2 = new OAuth2(cfg.CLIENT_ID, cfg.CLIENT_SECRET, server, 'oauth2/auth', 'oauth2/token', null);
+
+    console.log('redirecting');
+    res.redirect(oauth2.getAuthorizeUrl(authorizeConfig));
   });
-}
+
+  app.get('/callback', function(req, res) {
+    var code = req.query.code;
+
+    var server = 'https://accounts.google.com/o/';
+
+    var oauth2 = new OAuth2(cfg.CLIENT_ID, cfg.CLIENT_SECRET, server, 'oauth2/auth', 'oauth2/token', null);
+
+    accessParams = { 'grant_type': 'authorization_code',
+     'redirect_uri': 'http://localhost:3000/callback'
+    };
+
+    oauth2.getOAuthAccessToken(code, accessParams, function(err, access_token, refresh_token, results) {
+      if(err) {
+        console.log('Error: ');
+        console.log(err);
+      }
+      
+      console.log('Access Token: ');
+      console.log(access_token);
+      console.log('Refresh Token: ');
+      console.log(refresh_token);
+
+      console.log(results);
+      
+      if(refresh_token && access_token) {
+        // Success!!
+        res.send('success!');
+      }
+      else {
+        res.send('got access token: ' + access_token);
+      }
+    });
+  });
+
+  app.get('/xoauth', function(req, res) {
+
+  });
+
+  app.get('/mail', function(req, res) {
+
+    // Mail server //
+
+    var mailListener = new MailListener({
+      xoauth2: '',
+      username: cfg.MAIL_USERNAME,
+      password: cfg.MAIL_PASSWORD,
+      host: cfg.MAIL_HOST,
+      port: cfg.MAIL_PORT,
+      tls: true,
+      markSeen: false,            // Do not mark e-mail as read (yet!)
+      fetchUnreadOnStart: false,  // Do not grab all e-mail at once, only new stuff.
+      attachments: false          // Do not grab attachments (yet!)
+    });
+
+    console.log(mailListener);
+    mailListener.start();
+
+    mailListener.on('server:connected', function() {
+      console.log('imap Connected');
+    });
+
+    mailListener.on("server:disconnected", function(){
+      console.log("imap Disconnected");
+    });
+
+    mailListener.on("error", function(err){
+      console.log(err);
+    });
+
+    mailListener.on("mail", function(mail, seqno, attributes){
+      // do something with mail object including attachments
+      console.log("emailParsed", mail);
+      // mail processing code goes here
+    });
+
+    mailListener.on("attachment", function(attachment){
+      console.log('wtf, an attachment?!');
+      console.log(attachment.path);
+    });
+
+  });
+
+
+  app.listen(cfg.PORT);
+
+};
+
+
